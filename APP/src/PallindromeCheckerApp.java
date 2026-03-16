55f8c67839a913d1adfbab9abf05c6934c27968a
Shadow3478 <heytishv2006@gmail.com>
2026-03-16 16:35:08 +0530

    UC4  Character Array Based Validation

--- APP/src/PallindromeCheckerApp.java
+++ APP/src/PallindromeCheckerApp.java
@@ -6,16 +6,35 @@ public class PallindromeCheckerApp {
         String input = "madam";   // Hardcoded string
         String reversed = "";
 
-        // Iterate from last character to first
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        // Declare and initialize the input string
+        input = "radar";
+
+        // Convert the string into a character array
+        char[] chars = input.toCharArray();
+
+        // Initialize pointer at beginning
+        int start = 0;
+
+        // Initialize pointer at end
+        int end = chars.length - 1;
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Continue comparison until pointers cross
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
-        boolean isPalindrome = input.equals(reversed);
-
-        System.out.println ("Input text: " + input);
-        System.out.println ("Reversed text: " + reversed);
-        System.out.println ("Is it a Palindrome?: " + isPalindrome);
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
     }
 }
